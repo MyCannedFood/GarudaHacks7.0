28cfd4e1f393ce5ab2d97d72a0bc8d127978a773
rVerKotz <savero2605@gmail.com>
2026-07-13 23:43:45 +0700

    feat: Convert React App to JSX Syntax

--- resources/js/Page/Home/Home.jsx
+++ resources/js/Page/Home/Home.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-
 export default function Home() {
-    return React.createElement(
-        'div',
-        { style: { minHeight: '100vh', background: '#fff', padding: '2rem' } },
-        React.createElement('h1', { style: { fontSize: '2rem', fontWeight: 700 } }, 'Home'),
-        React.createElement('p', { style: { marginTop: '0.5rem', color: '#555' } }, 'Halaman Home')
+    return (
+        <div style={{ minHeight: '100vh', background: '#fff', padding: '2rem' }}>
+            <h1 style={{ fontSize: '2rem', fontWeight: 700 }}>Home</h1>
+            <p style={{ marginTop: '0.5rem', color: '#555' }}>Halaman Home</p>
+        </div>
     );
 }
